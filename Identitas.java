c6feb831b78528145e8f9712cfca437431574cdf
bungaaden <126657199+bungaaden@users.noreply.github.com>
2023-03-04 15:19:51 +0700

    Add files via upload

---
+++ Identitas.java
@@ -0,0 +1,10 @@
+public class Identitas
+{
+    public static void main (String[] args)
+    {
+        String nama = "Bunga Dinda Endri Novita";
+        System.out.println("Halo nama saya adalah "+nama);
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program java");
+    }
+}
