a1d628beea8f29b5b009e8243b8f5f9f0da47441
bungaaden <126657199+bungaaden@users.noreply.github.com>
2023-03-05 11:23:41 +0700

    Add files via upload

---
+++ Lingkaran.java
@@ -0,0 +1,9 @@
+public class Lingkaran
+{
+    public static void main (String[] args)
+    {
+        int d = 10;
+        double hasil = 3.14 * d;
+        System.out.print("Keliling lingkaran adalah "+hasil);
+    }
+}
---
+++ Tabung.java
@@ -0,0 +1,12 @@
+public class Tabung
+{
+    public static void main (String [] args)
+    {
+        double hasil, r, d, t;
+        d=5;
+        r=d/2;
+        t=10;
+        hasil = 3.14 * r * r * t;
+        System.out.print("Volume tabung adalah "+hasil);
+    }
+}
