cc9eef4f730f76c0689ffc689c787fc9c82dfffd
bungaaden <126657199+bungaaden@users.noreply.github.com>
2023-03-05 11:21:35 +0700

    Delete Lingkaran.java

--- Lingkaran.java
+++
@@ -1,9 +0,0 @@
-public class Lingkaran
-{
-    public static void main (String[] args)
-    {
-        int d = 10;
-        double hasil = 3.14 * d;
-        System.out.print("Keliling lingkaran adalah "+hasil);
-    }
-}
